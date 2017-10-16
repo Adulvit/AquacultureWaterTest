0181542ac234cdfd92d4cfbcbf3a9ebea2ec0715
Adulvitc <adulvitc@gmail.com>
2017-10-16 17:14:59 +0700

    Picture Auto Slide View Complete

--- app/src/main/java/com/betagro/adulvitc/aquaculturewatertest/MainActivity.java
+++ app/src/main/java/com/betagro/adulvitc/aquaculturewatertest/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -62,9 +65,41 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager.setAdapter(viewPagerAdapter);
 
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(), 2000,4000);
 
 
     }   //Main Method
+
+    public class MyTimerTask extends TimerTask {
+
+
+        @Override
+        public void run() {
+
+            MainActivity.this.runOnUiThread(new TimerTask() {
+                @Override
+                public void run() {
+
+                    if (viewPager.getCurrentItem() == 0) {
+                        viewPager.setCurrentItem(1);
+                    } else if (viewPager.getCurrentItem() == 1) {
+                        viewPager.setCurrentItem(2);
+                    } else if (viewPager.getCurrentItem() == 2) {
+                        viewPager.setCurrentItem(3);
+                    } else if (viewPager.getCurrentItem() == 3) {
+                        viewPager.setCurrentItem(4);
+                    } else if (viewPager.getCurrentItem() == 4) {
+                        viewPager.setCurrentItem(5);
+                    } else {
+                        viewPager.setCurrentItem(0);
+
+                    }
+                }
+            });
+
+        }
+    }
 
 
 }   //Main Class
