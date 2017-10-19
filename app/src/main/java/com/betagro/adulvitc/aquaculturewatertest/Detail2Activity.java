41bcef56a687fe08399389a89763e14048e4c421
Adulvitc <adulvitc@gmail.com>
2017-10-19 22:18:15 +0700

    DetailActivity3 complete

--- app/src/main/java/com/betagro/adulvitc/aquaculturewatertest/Detail2Activity.java
+++ app/src/main/java/com/betagro/adulvitc/aquaculturewatertest/Detail2Activity.java
@@ -11,66 +11,39 @@ import android.widget.TextView;
 
 public class Detail2Activity extends AppCompatActivity {
 
-    private ImageView innerImageView;
-    private EditText valueEditText;
-    private Button nextButton, backButton;
-    private TextView titleTextView, unitTextView,detailTextView;
+
+    private EditText doEditText,TempEditText,pHEditText,AlkEditText,
+            AmmoEditText,NiteEditText,HardEditText,TurbEditText;
+    private Button nextButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail2);
 
-        final ImageView innerImageView = (ImageView) findViewById(R.id.imgInner);
-        final EditText valueEditText = (EditText) findViewById(R.id.edtValue);
+
+        final EditText doEditText = (EditText) findViewById(R.id.edtValueDo);
+        final EditText TempEditText = (EditText) findViewById(R.id.edtValueTem);
+        final EditText pHEditText = (EditText) findViewById(R.id.edtValuepH);
+        final EditText AlkEditText = (EditText) findViewById(R.id.edtValueAlk);
+        final EditText AmmoEditText = (EditText) findViewById(R.id.edtValueAmmo);
+        final EditText NiteEditText = (EditText) findViewById(R.id.edtValueNite);
+        final EditText HardEditText = (EditText) findViewById(R.id.edtValueHard);
+        final EditText TurbEditText = (EditText) findViewById(R.id.edtValueTurb);
+
         final Button nextButton = (Button) findViewById(R.id.btnNext);
-        final Button backButton = (Button) findViewById(R.id.btnBack);
-        final TextView titleTextView = (TextView) findViewById(R.id.txtTitle);
-        final TextView unitTextView = (TextView) findViewById(R.id.txtUnit);
-        final TextView detailTextView = (TextView) findViewById(R.id.txtDetail);
 
-
-        final int imageInts = getIntent().getIntExtra("imgID",R.drawable.do2);
-        innerImageView.setImageResource(imageInts);
-
-        final int intIndex = getIntent().getIntExtra("Unit", 0);
-        String[] detailStrings = getResources().getStringArray(R.array.unit);
-        unitTextView.setText(detailStrings[intIndex]);
-
-        final int titleIndex = getIntent().getIntExtra("title", 0);
-        String[] titleStrings = getResources().getStringArray(R.array.title);
-        titleTextView.setText(titleStrings[titleIndex]);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
 
-        final int longDetail = getIntent().getIntExtra("longDetail", 0);
-        final String[] longStrings = getResources().getStringArray(R.array.long_detail);
-
-        final int subtitle = getIntent().getIntExtra("subtitle", 0);
-        String[] subtitleStrings = getResources().getStringArray(R.array.subtitle);
-
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-        /*nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Detail2Activity.this, DetailActivity.class);
-                intent.putExtra("title", titleIndex);
-                intent.putExtra("imgID", imageInts);
-                intent.putExtra("longDetail", longDetail);
-                intent.putExtra("subtitle", subtitle);
-
-                startActivity(intent);
 
 
-            }
-        });
-        */
+
+
+
+
 
 
     }   //Main Method
