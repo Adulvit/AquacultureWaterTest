119d79014c73a60b2f6dfb7cfe286157892cee4e
Adulvitc <adulvitc@gmail.com>
2017-10-26 11:51:55 +0700

    Header and Date,Time PieChart Activity Complete

--- app/src/main/java/com/betagro/adulvitc/aquaculturewatertest/PieChartActivity.java
+++ app/src/main/java/com/betagro/adulvitc/aquaculturewatertest/PieChartActivity.java
@@ -2,15 +2,11 @@ package com.betagro.adulvitc.aquaculturewatertest;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
-import android.text.style.RelativeSizeSpan;
-import android.text.style.StyleSpan;
+
 import android.widget.TextView;
-import com.github.mikephil.charting.animation.Easing;
+
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.PieData;
@@ -62,15 +58,13 @@ public class PieChartActivity extends AppCompatActivity {
         t.start();
 
 
-
         pieChart = (PieChart) findViewById(R.id.piechart);
 
-        pieChart.setUsePercentValues(true);
+        pieChart.setUsePercentValues(false);
         pieChart.getDescription().setEnabled(false);
         pieChart.setExtraOffsets(5, 10, 5, 5);
 
         pieChart.setDragDecelerationFrictionCoef(0.95f);
-
 
 
         pieChart.setDrawHoleEnabled(true);
@@ -80,11 +74,6 @@ public class PieChartActivity extends AppCompatActivity {
         pieChart.setCenterText("developed by Adulvit\nTechnical Service Division\nBetagro Group");
         pieChart.setCenterTextSize(13);
         pieChart.setTouchEnabled(false);
-
-
-
-
-
 
 
         ArrayList<PieEntry> yValue = new ArrayList<>();
@@ -99,40 +88,37 @@ public class PieChartActivity extends AppCompatActivity {
         yValue.add(new PieEntry(34f, "Turb"));
 
 
-
-
-
         Description description = new Description();
         description.setText("");
         description.setTextSize(50);
         pieChart.setDescription(description);
 
-        pieChart.animateY(100, Easing.EasingOption.EaseInOutCubic);
+        pieChart.animateY(1500);
 
+//        setting PieChart
         PieDataSet dataSet = new PieDataSet(yValue, "");
         dataSet.setSliceSpace(1f);
         dataSet.setSelectionShift(5f);
         dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        //dataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
 
+
+//        Set Value in Other pie
         PieData data = new PieData(dataSet);
-        data.setValueTextSize(10f);
+        data.setValueTextSize(0f);
         data.setValueTextColor(Color.YELLOW);
-
         pieChart.setData(data);
 
 
-
         ArrayList<Integer> colors = new ArrayList<>();
-        colors.add(Color.rgb(79,129,189));
-        colors.add(Color.rgb(0,176,80));
-        colors.add(Color.rgb(0,176,240));
-        colors.add(Color.rgb(247,150,70));
-        colors.add(Color.rgb(128,100,162));
-        colors.add(Color.rgb(255,0,0));
-        colors.add(Color.rgb(155,187,89));
-        colors.add(Color.rgb(64,64,64));
-
-
+        colors.add(Color.rgb(79, 129, 189));
+        colors.add(Color.rgb(0, 176, 80));
+        colors.add(Color.rgb(0, 176, 240));
+        colors.add(Color.rgb(247, 150, 70));
+        colors.add(Color.rgb(128, 100, 162));
+        colors.add(Color.rgb(255, 0, 0));
+        colors.add(Color.rgb(155, 187, 89));
+        colors.add(Color.rgb(64, 64, 64));
         dataSet.setColors(colors);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -149,16 +135,7 @@ public class PieChartActivity extends AppCompatActivity {
         PondTextView.setText(Pondstr);
 
 
-
-
-
-
-
-
-
     }   //Main Method
-
-
 
 
 }   //Main Class
