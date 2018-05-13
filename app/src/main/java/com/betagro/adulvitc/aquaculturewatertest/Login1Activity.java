95588049d4147ce753120623aba2a44192dc8b79
Adulvitc <adulvitc@gmail.com>
2018-05-13 13:49:46 +0700

    Upload and download User to server complete

--- app/src/main/java/com/betagro/adulvitc/aquaculturewatertest/Login1Activity.java
+++ app/src/main/java/com/betagro/adulvitc/aquaculturewatertest/Login1Activity.java
@@ -1,28 +1,38 @@
 package com.betagro.adulvitc.aquaculturewatertest;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.betagro.adulvitc.aquaculturewatertest.utility.MyGetAllData;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Login1Activity extends AppCompatActivity {
 
     private EditText userEditText, passEditText;
     private TextView registryTextView;
     private Button loginButton;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login1);
 
-        EditText userEditText = (EditText) findViewById(R.id.etUsername);
-        EditText passEditText = (EditText) findViewById(R.id.etPassword);
+
         TextView registryTextView = (TextView) findViewById(R.id.tvRegisterLink);
         Button loginButton = (Button) findViewById(R.id.bSignIn);
+
 
 
         registryTextView.setOnClickListener(new View.OnClickListener() {
@@ -36,9 +46,104 @@ public class Login1Activity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login1Activity.this, MainActivity.class);
-                startActivity(intent);
+
+                final EditText userEditText = (EditText) findViewById(R.id.edtUsername);
+                final EditText passEditText = (EditText) findViewById(R.id.edtPassword);
+
+                userString = userEditText.getText().toString().trim();
+                passwordString = passEditText.getText().toString().trim();
+
+                final AlertDialog.Builder ad = new AlertDialog.Builder(Login1Activity.this);
+
+                if (userString.equals("") || passwordString.equals("")) {
+//                  Have Space
+                    ad.setTitle("Have Space");
+                    ad.setCancelable(false);
+                    ad.setIcon(R.drawable.icon_alert);
+                    //ad.setNegativeButton("Close", null);
+                    ad.setMessage("Please Fill All");
+                    ad.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+
+                        }
+                    });
+                    ad.show();
+                    return;
+
+
+                } else {
+//                  No Space
+                    try {
+
+                        boolean status = true;  // true ---> user False
+                        String truePasswordString = null,nameString = null;
+
+                        MyGetAllData myGetAllData = new MyGetAllData(getApplication());
+                        myGetAllData.execute("http://androidthai.in.th/sky/getAllDataAdulvitC.php");
+                        String jsonString = myGetAllData.get();
+
+                        Log.d("AdulvitC", "JSON" + jsonString);
+
+                        JSONArray jsonArray = new JSONArray(jsonString);
+                        for (int i=0; i<jsonArray.length(); i+=1) {
+
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            if (userString.equals(jsonObject.getString("User"))) {
+                                status = false;
+                                truePasswordString = jsonObject.getString("Password");
+                                nameString = jsonObject.getString("Name");
+
+                            }
+
+                        }
+
+                        if (status) {
+                            ad.setTitle("User False");
+                            ad.setCancelable(false);
+                            ad.setIcon(R.drawable.icon_alert);
+                            ad.setNegativeButton("Close", null);
+                            ad.setMessage("No this User in my Database");
+                            ad.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+
+                        } else if (passwordString.equals(truePasswordString)) {
+                            Toast.makeText(getApplication(), "Welcome" + nameString, Toast.LENGTH_SHORT);
+                        } else {
+                            ad.setTitle("Password False");
+                            ad.setCancelable(false);
+                            ad.setIcon(R.drawable.icon_alert);
+                            ad.setNegativeButton("Close", null);
+                            ad.setMessage("Please Try again Password False");
+                            ad.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+
+                        }
+
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+
+
+                }
+
+
             }
+
+
         });
 
 
